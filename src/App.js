f415e7049d9e23f26e0f1543323c090ee9483d4b
Jean C. Contreras <jean.cntrs@gmail.com>
2020-12-08 12:27:35 -0300

    Project was cleaned and first components were created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BandAdd from './components/BandAdd';
+import BandList from './components/BandList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="container">
+            <div className="alert">
+                <p>
+                    Service status:
+                    <span className="text-success"> Online</span>
+                    <span className="text-danger"> Offline</span>
+                </p>
+            </div>
+            <h1>Band Names</h1>
+            <hr />
+            <div className="row">
+                <div className="col-8">
+                    <BandList />
+                </div>
+                <div className="col-4">
+                    <BandAdd />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
