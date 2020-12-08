46b50d009c0d57db289488ad7a729653eb186ed7
Jean C. Contreras <jean.cntrs@gmail.com>
2020-12-08 18:43:48 -0300

    Was added the function to rename band name

--- src/components/BandList.js
+++ src/components/BandList.js
@@ -2,28 +2,52 @@ import React, { useState, useEffect } from 'react';;
 
 const BandList = ({ data }) => {
     const [bands, setBands] = useState(data);
-    console.log(data)
-    useEffect(() => {
-        setBands(data);
-    }, [data]);
+
+    const handleBandNameChange = (event, id) => {
+        const newBandName = event.target.value;
+
+        setBands(bands => bands.map(band => {
+            if (band.id === id) {
+                band.name = newBandName;
+            }
+
+            return band;
+        }));
+    }
+
+    const handleLostFocus = (id, name) => {
+        console.log(id, name)
+    }
 
     const createRows = () => {
         return (
             bands.map(band => (
                 <tr key={band.id}>
-                    <td><button className="btn btn-primary">+1</button></td>
+                    <td>
+                        <button className="btn btn-primary">+1</button>
+                    </td>
                     <td>
                         <input
                             className="form-control"
                             value={band.name}
+                            onChange={event => handleBandNameChange(event, band.id)}
+                            onBlur={() => handleLostFocus(band.id, band.name)}
                         />
                     </td>
-                    <td><h3>15</h3></td>
-                    <td><button className="btn btn-danger">Delete</button></td>
+                    <td>
+                        <h3>{band.votes}</h3>
+                    </td>
+                    <td>
+                        <button className="btn btn-danger">Delete</button>
+                    </td>
                 </tr>
             ))
         );
     }
+
+    useEffect(() => {
+        setBands(data);
+    }, [data]);
 
     return (
         <>
